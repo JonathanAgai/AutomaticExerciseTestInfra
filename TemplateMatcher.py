f1821f404a4ad4567cae2a30af6f9a2e4b601133
DESKTOP-6O7R7IJ\Ben <yonilol22@gmail.com>
2021-11-30 17:48:21 +0200

    add draw_rectangle_around_target fucntion for templatematcher class add save_result_image function for templatematcher class

--- TemplateMatcher.py
+++ TemplateMatcher.py
@@ -8,6 +8,20 @@ class TemplateMatcher:
         self.source_image = source_image
         self.crop_image = crop_image
         self.test_name = test_name
+        self.match_found = False
+
+    def draw_rectangle_around_target(self, loc, image, w, h):
+        # for pt in zip(*loc[::-1]):
+        # is for the points which have values greater than threshold.
+        # zip is a container of all such points
+        # and it will iterate to all such points
+        # and draw rectangle around this closed entity.
+        for pt in zip(*loc[::-1]):
+            cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+    def save_result_image(self, image):
+        result_path = 'C:\\Users\\Ben\\Desktop\\template_results' + f'\\{self.test_name}.png'
+        result_image = cv2.imwrite(result_path, image)
 
     def template_matching(self):
         image = cv2.imread(self.source_image)
@@ -17,12 +31,13 @@ class TemplateMatcher:
 
         res = cv2.matchTemplate(image_gray, template, cv2.TM_CCOEFF_NORMED)
         threshold = 0.99
+
+        if np.amax(res) >= threshold:
+            self.match_found = True
+
         loc = np.where(res >= threshold)
-        for pt in zip(*loc[::-1]):
-            cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
-        result_path = 'C:\\Users\\Ben\\Desktop\\template_results' + f'\\{self.test_name}.png'
-        match_found = cv2.imwrite(result_path, image)
-        cv2.waitKey(0)
+        self.draw_rectangle_around_target(loc, image, w, h)
+        self.save_result_image(image)
 
-        return match_found
+        return self.match_found
